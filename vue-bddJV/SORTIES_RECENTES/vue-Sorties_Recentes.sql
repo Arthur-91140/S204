858613ba2b867cabeb9d749abfaaf990eeedb019
Arthur-91140 <96424611+Arthur-91140@users.noreply.github.com>
2025-05-25 21:39:43 +0200

    Refactor SORTIES_RECENTES view to improve code readability by adding comments and adjusting formatting

--- vue-bddJV/SORTIES_RECENTES/vue-Sorties_Recentes.sql
+++ vue-bddJV/SORTIES_RECENTES/vue-Sorties_Recentes.sql
@@ -1,8 +1,14 @@
-CREATE OR REPLACE VIEW SORTIES_RECENTES AS
-SELECT J.idJeu, J.titreJeu, DS.datesortie, LISTAGG(DISTINCT P.nomplateforme, ', ') WITHIN GROUP (ORDER BY P.nomplateforme) AS plateforme
+SELECT 
+J.idJeu, 
+J.titreJeu, 
+DS.datesortie, 
+-- toutes les plateformes
+LISTAGG(DISTINCT P.nomplateforme, ', ') WITHIN GROUP (ORDER BY P.nomplateforme) AS plateforme
 FROM jeu J
 INNER JOIN datesortie DS ON ds.idJeu = j.idJeu
 INNER JOIN plateforme P ON P.idplateforme = DS.idplateforme
+-- que les jeux deja sortis
 WHERE DS.datesortie <= SYSDATE
 GROUP BY J.idJeu, J.titreJeu, DS.datesortie
+-- tri par date
 ORDER BY DS.datesortie DESC, J.titrejeu ASC;
